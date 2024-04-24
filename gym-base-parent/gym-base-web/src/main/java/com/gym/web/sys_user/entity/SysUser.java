c8155f2949111c5de02329cf3600afa06fb14bfe
BoyHanYang <480526527@qq.com>
2024-04-25 03:08:51 +0800

    解决账户无法登录，出现账户被锁定的情况，将默认值改为true即可

--- gym-base-parent/gym-base-web/src/main/java/com/gym/web/sys_user/entity/SysUser.java
+++ gym-base-parent/gym-base-web/src/main/java/com/gym/web/sys_user/entity/SysUser.java
@@ -117,21 +117,21 @@ public class SysUser implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
